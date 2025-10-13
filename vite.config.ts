2644bd5382d905ad3693137b2429f4141a11c4d2
aviralmittal8 <aviralmittal08@gmail.com>
2025-10-13 11:25:52 +0530

    Refractor: Dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Pregatips/",  // <-- Add this line for GitHub Pages
   server: {
     host: "::",
     port: 8080,
